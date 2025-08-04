eef68595a41368c007ddc108eacc91f873addabe
Rafael Menegheti <rafaelmenegheti51@gmail.com>
2025-08-04 14:55:18 -0300

    chore: update shadcn theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { cn } from "@/lib/utils";
 const Home = () => {
   return (
     <div>
-      <Button className="my-1 bg-red-500 px-5">Bootcamp</Button>
+      <Button className="">Bootcamp</Button>
       <button className="bg-amber-800">Hello World</button>
     </div>
   );

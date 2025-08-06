74b7b57f90ed6b30e09f9563529fdefcfd140c0c
Rafael Menegheti <rafaelmenegheti51@gmail.com>
2025-08-06 18:21:05 -0300

    chore: add cart table

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -14,6 +14,7 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "../ui/sheet";
+import { Cart } from "./cart";
 
 export const Header = () => {
   const { data: session } = authClient.useSession();
@@ -24,7 +25,7 @@ export const Header = () => {
         <Image src={"/logo.svg"} alt="BEWEAR" width={100} height={26.14} />
       </Link>
 
-      <div className="flex items-center">
+      <div className="flex items-center gap-3">
         <Sheet>
           <SheetTrigger asChild>
             <Button variant={"outline"} size={"icon"}>
@@ -77,6 +78,7 @@ export const Header = () => {
             </div>
           </SheetContent>
         </Sheet>
+        <Cart />
       </div>
     </header>
   );

ae17d566f64e56845d29650a617de0f30348a0cf
Rafael Menegheti <rafaelmenegheti51@gmail.com>
2025-08-06 15:24:27 -0300

    feat: add category products page

--- src/components/common/product-item.tsx
+++ src/components/common/product-item.tsx
@@ -3,29 +3,35 @@ import Link from "next/link";
 
 import { productTable, productVariantTable } from "@/db/schema";
 import { formatCentsToBRL } from "@/helpers/money";
+import { cn } from "@/lib/utils";
 
 interface Props {
   product: typeof productTable.$inferSelect & {
     variants: (typeof productVariantTable.$inferSelect)[];
   };
+  texteContainerClassname?: string;
 }
 
-const ProductItem = ({ product }: Props) => {
+const ProductItem = ({ product, texteContainerClassname }: Props) => {
   const firstVariant = product.variants[0];
   console.log({ firstVariant });
 
   return (
     <Link href={"/"} className="flex flex-col gap-4">
       <Image
-        src={
-          "https://fsc-projects-static.s3.us-east-1.amazonaws.com/BEWEAR/products/Acesso%CC%81rios/1/78f9fa3b_c793_472a_b183_e32495033da2.jpg"
-        }
+        src={firstVariant.imageUrl}
         alt={firstVariant.name}
-        width={200}
-        height={200}
-        className="rounded-3xl"
+        sizes="100vw"
+        height={0}
+        width={0}
+        className="h-auto w-full rounded-3xl"
       />
-      <div className="flex max-w-[200px] flex-col gap-1">
+      <div
+        className={cn(
+          "flex max-w-[200px] flex-col gap-1",
+          texteContainerClassname,
+        )}
+      >
         <p className="truncate text-sm font-medium">{product.name}</p>
         <p className="text-sx text-muted-foreground truncate font-medium">
           {product.description}

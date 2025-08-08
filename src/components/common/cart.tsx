0034c0ca9fb677334d632899306d14d36caafd3a
Rafael Menegheti <rafaelmenegheti51@gmail.com>
2025-08-08 17:04:55 -0300

    feat: ensure user addresses are shown in idewntification page

--- src/components/common/cart.tsx
+++ src/components/common/cart.tsx
@@ -16,6 +16,7 @@ import {
 } from "../ui/sheet";
 import CartItem from "./cart-item";
 import { useCart } from "@/hooks/queries/use-cart";
+import Link from "next/link";
 
 export const Cart = () => {
   const { data: cart } = useCart()
@@ -76,7 +77,11 @@ export const Cart = () => {
                 <p>{formatCentsToBRL(cart?.totalPriceInCents ?? 0)}</p>
               </div>
 
-              <Button className="mt-5 rounded-full">Finalizar compra</Button>
+              <Button className="mt-5 rounded-full" asChild>
+                <Link href={"/cart/identification"}>
+                  Finalizar compra
+                </Link>
+              </Button>
             </div>
           )}
         </div>
